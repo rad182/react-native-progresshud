8aa666bb3483d1bfbd012b26aeb46da07c53a0b7
gongkai <865425695@qq.com>
2017-08-09 19:46:11 +0800

    update

--- RNProgressHUD/RNProgressHUD/RNProgressHUD.m
+++ RNProgressHUD/RNProgressHUD/RNProgressHUD.m
@@ -20,7 +20,7 @@ typedef NS_ENUM(NSInteger, RNProgressHUDMode) {
 @interface RNProgressHUD()
 
 
-@property (nonatomic,strong) MBProgressHUD *hub;
+@property (nonatomic,strong) MBProgressHUD *hud;
 @property (nonatomic,strong) UIWindow *window;
 
 
@@ -43,16 +43,19 @@ RCT_EXPORT_METHOD(showSimpleText:(NSString *)message duration:(NSInteger)duratio
 {
     
     dispatch_async(dispatch_get_main_queue(), ^{
-        if(self.hub){
-            [self.hub hideAnimated:YES];
+        if(self.hud){
+            [self.hud hideAnimated:YES];
         }
-        self.hub = [MBProgressHUD showHUDAddedTo:self.window animated:YES];
-        self.hub.mode = MBProgressHUDModeText;
-        self.hub.label.text = message;
-        
-        if (self.hub) {
-            [self.hub hideAnimated:YES afterDelay:duration / 1000];
-            self.hub = NULL;
+        self.hud = [MBProgressHUD showHUDAddedTo:self.window animated:YES];
+        self.hud.mode = MBProgressHUDModeText;
+        self.hud.label.text = message;
+        self.hud.bezelView.color = [UIColor blackColor];
+        self.hud.bezelView.style = MBProgressHUDBackgroundStyleSolidColor;
+        self.hud.bezelView.opaque = 0.8;
+        self.hud.label.textColor = [UIColor whiteColor];
+        if (self.hud) {
+            [self.hud hideAnimated:YES afterDelay:duration / 1000];
+            self.hud = NULL;
         }
     });
     
@@ -60,57 +63,73 @@ RCT_EXPORT_METHOD(showSimpleText:(NSString *)message duration:(NSInteger)duratio
 RCT_EXPORT_METHOD(showSpinIndeterminate)
 {
     dispatch_async(dispatch_get_main_queue(), ^{
-        if(self.hub){
-            [self.hub hideAnimated:YES];
+        if(self.hud){
+            [self.hud hideAnimated:YES];
         }
-        self.hub = [MBProgressHUD showHUDAddedTo:self.window animated:YES];
-        self.hub.mode = MBProgressHUDModeIndeterminate;
+        self.hud = [MBProgressHUD showHUDAddedTo:self.window animated:YES];
+        self.hud.mode = MBProgressHUDModeIndeterminate;
+        self.hud.bezelView.color = [UIColor blackColor];
+        self.hud.bezelView.style = MBProgressHUDBackgroundStyleSolidColor;
+        self.hud.bezelView.opaque = 0.8;
+        self.hud.label.textColor = [UIColor whiteColor];
     });
 }
 RCT_EXPORT_METHOD(showSpinIndeterminateWithTitle:(NSString *)title)
 {
 
     dispatch_async(dispatch_get_main_queue(), ^{
-        if(self.hub){
-            [self.hub hideAnimated:YES];
+        if(self.hud){
+            [self.hud hideAnimated:YES];
         }
-        self.hub = [MBProgressHUD showHUDAddedTo:self.window animated:YES];
-        self.hub.mode = MBProgressHUDModeIndeterminate;
-        self.hub.label.text = title;
+        self.hud = [MBProgressHUD showHUDAddedTo:self.window animated:YES];
+        self.hud.mode = MBProgressHUDModeIndeterminate;
+        self.hud.label.text = title;
+        self.hud.bezelView.color = [UIColor blackColor];
+        self.hud.bezelView.style = MBProgressHUDBackgroundStyleSolidColor;
+        self.hud.bezelView.opaque = 0.8;
+        self.hud.label.textColor = [UIColor whiteColor];
     });
     
 }
 RCT_EXPORT_METHOD(showSpinIndeterminateWithTitleAndDetails:(NSString *)title content:(NSString *)details)
 {
     dispatch_async(dispatch_get_main_queue(), ^{
-        if(self.hub){
-            [self.hub hideAnimated:YES];
+        if(self.hud){
+            [self.hud hideAnimated:YES];
         }
-        self.hub = [MBProgressHUD showHUDAddedTo:self.window animated:YES];
-        self.hub.mode = MBProgressHUDModeIndeterminate;
-        self.hub.label.text = title;
-        self.hub.detailsLabel.text = details;
+        self.hud = [MBProgressHUD showHUDAddedTo:self.window animated:YES];
+        self.hud.mode = MBProgressHUDModeIndeterminate;
+        self.hud.label.text = title;
+        self.hud.detailsLabel.text = details;
+        self.hud.bezelView.color = [UIColor blackColor];
+        self.hud.bezelView.style = MBProgressHUDBackgroundStyleSolidColor;
+        self.hud.bezelView.opaque = 0.8;
+        self.hud.label.textColor = [UIColor whiteColor];
     });
 }
 RCT_EXPORT_METHOD(showDeterminate:(NSInteger *)mode title:(NSString *)title details:(NSString *)details)
 {
     dispatch_async(dispatch_get_main_queue(), ^{
-        if(self.hub){
-            [self.hub hideAnimated:YES];
+        if(self.hud){
+            [self.hud hideAnimated:YES];
         }
-        self.hub = [MBProgressHUD showHUDAddedTo:self.window animated:YES];
-    
+        self.hud = [MBProgressHUD showHUDAddedTo:self.window animated:YES];
+        self.hud.bezelView.color = [UIColor blackColor];
+        self.hud.bezelView.style = MBProgressHUDBackgroundStyleSolidColor;
+        self.hud.bezelView.opaque = 0.8;
+        self.hud.label.textColor = [UIColor whiteColor];
+        
         if(mode == RNProgressHUDModeAnnularDeterminate){
-            self.hub.mode = MBProgressHUDModeAnnularDeterminate;
+            self.hud.mode = MBProgressHUDModeAnnularDeterminate;
         } else if(mode == RNProgressHUDModeDeterminateHorizontalBar){
-            self.hub.mode = MBProgressHUDModeDeterminateHorizontalBar;
+            self.hud.mode = MBProgressHUDModeDeterminateHorizontalBar;
         }
         
         if (title) {
-            self.hub.label.text = title;
+            self.hud.label.text = title;
         }
         if(details){
-            self.hub.detailsLabel.text = details;
+            self.hud.detailsLabel.text = details;
         }
      
     });
@@ -119,7 +138,7 @@ RCT_EXPORT_METHOD(showDeterminate:(NSInteger *)mode title:(NSString *)title deta
 RCT_EXPORT_METHOD(setProgress:(CGFloat)progress )
 {
     dispatch_async(dispatch_get_main_queue(), ^{
-        if (_hub) {
+        if (_hud) {
             [MBProgressHUD HUDForView:self.window].progress = progress;
         }
     });
@@ -128,9 +147,9 @@ RCT_EXPORT_METHOD(setProgress:(CGFloat)progress )
 RCT_EXPORT_METHOD(dismiss)
 {
     dispatch_async(dispatch_get_main_queue(), ^{
-        if(self.hub){
-            [self.hub hideAnimated:YES];
-            self.hub = NULL;
+        if(self.hud){
+            [self.hud hideAnimated:YES];
+            self.hud = NULL;
         }
     });
 }
